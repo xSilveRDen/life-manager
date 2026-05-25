9abd78963ddfb94b8054f1d9a4c094f75df81e45
xSilveRDen <rden.ray49@gmail.com>
2026-05-25 23:33:49 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXZT2RVuVUS0qmflfgHzgROY30Ebg3uIQ",
-  authDomain: "life-manager-17332.firebaseapp.com",
-  databaseURL: "https://life-manager-17332-default-rtdb.firebaseio.com",
-  projectId: "life-manager-17332",
-  storageBucket: "life-manager-17332.firebasestorage.app",
-  messagingSenderId: "941800018409",
-  appId: "1:941800018409:web:cbf337c7ad192408817b78"
+  apiKey: "AIzaSyACigrDT1_f5aXn83_vY9FuxrfMx-jfB_8",
+  authDomain: "life-manager-97a23.firebaseapp.com",
+  databaseURL: "https://life-manager-97a23-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "life-manager-97a23",
+  storageBucket: "life-manager-97a23.firebasestorage.app",
+  messagingSenderId: "319273793166",
+  appId: "1:319273793166:web:75667ba79f930bb26f7bdf"
 }
 
 const app = initializeApp(firebaseConfig)
